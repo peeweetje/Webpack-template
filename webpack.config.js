d0dd1fcb43072bb039a5f8b958e42841c95443a3
Petra Vos <petravos70@gmail.com>
2017-12-15 22:02:33 +0100

    refactor: added module to file

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ let config = {
     output: {
         filename: 'output.js'
     }
-}
+},
+module: {
+    rules: [{
+      test: /\.js$/, // files ending with .js
+      exclude: /node_modules/, // exclude the node_modules directory
+      loader: "babel-loader" // use this (babel-core) loader
+    }]
+  }
 
 module.exports = config;
+
+
