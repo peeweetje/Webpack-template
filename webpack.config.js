7f5706a2b698f142b75efbe0957db5a073aecb99
Petra Vos <petravos70@gmail.com>
2017-12-15 22:38:04 +0100

    refactor: added style loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ let config = {
         test: /\.js$/, // files ending with .js
         exclude: /node_modules/, // exclude the node_modules directory
         loader: "babel-loader" // use this (babel-core) loader
+      },
+      {
+        test: /\.scss$/, //files ending with .scss
+        loader: ["style-loader", "css-loader", "sass-loader"] //use these loaders
       }
     ]
   }
