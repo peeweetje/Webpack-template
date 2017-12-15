95c0490f1bb308ff5f585ba2c3f124bd7c95936e
Petra Vos <petravos70@gmail.com>
2017-12-15 22:26:16 +0100

    fix: fixed bug in module

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
-const webpack = require('webpack');
-
+const webpack = require("webpack");
 
 let config = {
-    entry: './index.js',
-    output: {
-        filename: 'output.js'
-    }
-},
-module: {
-    rules: [{
-      test: /\.js$/, // files ending with .js
-      exclude: /node_modules/, // exclude the node_modules directory
-      loader: "babel-loader" // use this (babel-core) loader
-    }]
+  entry: "./index.js",
+  output: {
+    filename: "output.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files ending with .js
+        exclude: /node_modules/, // exclude the node_modules directory
+        loader: "babel-loader" // use this (babel-core) loader
+      }
+    ]
   }
+};
 
 module.exports = config;
-
-
